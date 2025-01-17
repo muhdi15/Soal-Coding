4fba794f73fdceed827e25cfc9dfae1d0012b9b9
muhdi15 <muhdimuhdi15@gmail.com>
2025-01-17 11:48:42 +0800

    Tambahan Soal Baru

---
+++ soal4.py
@@ -0,0 +1,10 @@
+data = ["data","file","informasi","memory","save"]
+
+for i in data:
+    if(i == "data"):
+        print(f"data Found = {i}")
+        break
+
+    else:
+        print("data not found")
+        break
